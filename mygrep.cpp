a052f66cc97fc268f6b1aa8c7fbd11cf5f347d3b
KippaK <antti.venetjoki@gmail.com>
2023-02-10 16:46:27 +0200

    Refactored parts of the program

--- mygrep.cpp
+++ mygrep.cpp
@@ -12,6 +12,24 @@ struct Line {
     int lineNum;
 };
 
+void strToLower(string &str){
+    transform(str.begin(), str.end(), str.begin(),
+        [](unsigned char c){ return std::tolower(c); });
+}
+
+bool validOptions(string options){
+    if (options[0] != '-' || options[1] != 'o'){
+        cout << "Options not recognized.";
+        return false;
+    }
+    options.erase(0,2);
+    if (options.find_first_not_of("ilor") != std::string::npos){
+        cout << "Options not recognized.";
+        return false;
+    }
+    return true;
+}
+
 void printLines(vector<Line> lines, bool printLineNumber, bool printOccurenceCount, bool reverseSearch, bool ignoreCase, string search){
     if (lines.size() == 0){
         cout << "No occurences of lines containing \"" << search << "\"";
@@ -42,10 +60,9 @@ void noArgs(){
     getline(cin, str);
     cout << "Give search string: ";
     getline(cin, search);
-    
-    size_t found = str.find(search);
-    if (found!=string::npos){
-        cout << "\"" << search << "\" found in \"" << str << "\" in postion " << found;
+
+    if (str.find(search) != string::npos){
+        cout << "\"" << search << "\" found in \"" << str << "\" in postion " << str.find(search);
     }
     else {
         cout << "\"" << search << "\" NOT found in \"" << str << "\"";
@@ -54,18 +71,30 @@ void noArgs(){
 }
 
 void searchStrFromFile(string options, string search, string fileName){
-    if (!filesystem::exists(fileName)){
-        cout << "File \"" << fileName << "\" doesn't exist in working directory.";
-        return;
-    }
+    
+    fstream file;
+    file.open(fileName, ios::in);
+    vector<Line> linesFound;
+    string line;
+    int lineCount = 1;
+    Line lineInfo;
+
+    string searchOriginal = search;
+    string lineOriginal;
 
     bool printLineNum = false;
     bool printOccurenceCount = false;
     bool reverseSearch = false;
     bool ignoreCase = false;
 
-    if (options.length() > 2){
-        options.erase(0,2);
+    if (!filesystem::exists(fileName)){
+        cout << "File \"" << fileName << "\" doesn't exist in working directory.";
+        return;
+    }
+    if (!validOptions(options)){ return; }
+    options.erase(0, 2);
+
+    if (options.length() > 0){
         if (options.find('l') != string::npos){
             printLineNum = true;
         }
@@ -80,44 +109,30 @@ void searchStrFromFile(string options, string search, string fileName){
         }
     }
 
-    fstream file;
-    file.open(fileName, ios::in);
+    while (getline(file, line)){
+        lineOriginal = line;
 
-    if (file.is_open()){
-        vector<Line> linesFound;
-        string line;
-        int lineCount = 1;
-        Line lineInfo;
-
-        string searchOriginal = search;
-        string lineOriginal;
-        while (getline(file, line)){
-            lineOriginal = line;
-
-            if (ignoreCase){
-                transform(search.begin(), search.end(), search.begin(),
-                    [](unsigned char c){ return std::tolower(c); });
-
-                transform(line.begin(), line.end(), line.begin(),
-                    [](unsigned char c){ return std::tolower(c); });
-            }
-
-            if (line.find(search) != string::npos && reverseSearch == false){
-                lineInfo.line = lineOriginal;
-                lineInfo.lineNum = lineCount;
-                linesFound.push_back(lineInfo);
-            }
-            else if (line.find(search) == string::npos && reverseSearch == true){
-                lineInfo.line = lineOriginal;
-                lineInfo.lineNum = lineCount;
-                linesFound.push_back(lineInfo);
-            }
-            lineCount++;
+        if (ignoreCase){
+            strToLower(search);
+            strToLower(line);
         }
 
-        printLines(linesFound, printLineNum, printOccurenceCount, reverseSearch, ignoreCase, search);
+        if (line.find(search) != string::npos && reverseSearch == false){
+            lineInfo.line = lineOriginal;
+            lineInfo.lineNum = lineCount;
+            linesFound.push_back(lineInfo);
+        }
+        else if (line.find(search) == string::npos && reverseSearch == true){
+            lineInfo.line = lineOriginal;
+            lineInfo.lineNum = lineCount;
+            linesFound.push_back(lineInfo);
+        }
+        lineCount++;
     }
+
+        printLines(linesFound, printLineNum, printOccurenceCount, reverseSearch, ignoreCase, searchOriginal);
 }
+
 
 int main(int argc, char **argv){
     if (argc == 1){
